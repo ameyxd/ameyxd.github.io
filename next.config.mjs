e40a5c37fa71c687bb3c13e9d7b85391b392679f
Amey Ambade <ameyambade@gmail.com>
2024-10-22 17:59:58 -0500

    Configure Next.js for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // basePath: "/portfolio", # TODO: Add domain name
 };
 
 export default nextConfig;
